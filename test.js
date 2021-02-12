d7baaf34e66faf65d2c4062b289ef82871d70a7c
Stefan Linsmayer <stefanlinsmayer@web.de>
2021-02-12 16:24:24 +0100

    jQuery wurde eingebuden

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
 'use strict';
 
 window.onload = function () {
-    
+    $("button.continue").html("Next Step...");
+    $("button.continue").on( "click",  function() {
+        alert("hiho")
+    });
 }
