2d4ba3eabda1e39b76d3235d7f55b7a7e634abfd
Stefan Linsmayer <stefanlinsmayer@web.de>
2021-02-12 14:32:37 +0100

    rechner wurde erweitert

--- rechner.js
+++ rechner.js
@@ -1,6 +1,13 @@
 //rechner-function
 'use strict'
 
-function rechner (a,b,operator) {
+function rechner(a, b, operator) {
+    if (operator === '+') {
+        return a + b;
+    } else if (operator === '-') {
+        return a - b;
+    } else if (operator === '*') {
+        return a * b;
+    }
 
 }
